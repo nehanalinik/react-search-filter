cc91e2249aba6093a6f7aa610623b08f786c9f9e
nehanalinik <nehanalinik@gmail.com>
2022-03-25 14:55:47 +0530

    first commit react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import jsonData from "./MOCK_DATA.json";
 
 function App() {
+  const [search, setSearch] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+      {jsonData
+        .filter((val) => {
+          if (search === "") {
+            return val;
+          } else if (
+            val.first_name.toLowerCase().includes(search.toLowerCase())
+          ) {
+            return val;
+          }
+        })
+        .map((item) => (
+          <div key={item.id} className="user">
+            <p>{item.first_name}</p>
+          </div>
+        ))}
     </div>
   );
 }
